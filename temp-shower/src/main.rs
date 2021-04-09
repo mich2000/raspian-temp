c797242025d42534fd0d2e92d2a0cbd995ffbdfc
Michael Hertsens <michael28072000@outlook.com>
2021-04-09 15:23:00 +0200

    update temp shower

--- temp-shower/src/main.rs
+++ temp-shower/src/main.rs
@@ -9,9 +9,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
     let mut bus = I2c::with_bus(1)?;
     bus.set_slave_address(0x44)?;
-    bus.block_write(0x2c, [0x06])?;
-    sleep(Duration::from_secs(1));
+    bus.smbus_block_read(0x2c, [0x06])?;
+    sleep(Duration::from_micros(500_000));
     let mut bytes = Vec::new();
-    bus.block_read(0x00, &bytes)?;
+    bus.smbus_block_read(0x44, &bytes)?;
     Ok(())
 }
