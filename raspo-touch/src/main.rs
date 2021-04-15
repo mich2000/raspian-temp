3f84d781bd0b20c952d11c39e20b5b07b086770c
Michael Hertsens <michael28072000@outlook.com>
2021-04-15 22:05:13 +0100

    update with custom gpio_zero lib

--- raspo-touch/src/main.rs
+++ raspo-touch/src/main.rs
@@ -3,11 +3,11 @@ use rust_gpiozero::DigitalInputDevice;
 use std::time::Duration;
 
 fn main() {
-    let pin_number = args().next().unwrap();
-    let pin_number = pin_number.parse::<u16>().unwrap();
+    let pin_number = args().nth(1).unwrap();
+    let pin_number = pin_number.parse::<u8>().unwrap();
     let mut device = DigitalInputDevice::new(pin_number);
     loop {
-        device.wait_for_active(Some(Duration::from_secs(5)));
+        device.wait_for_active(Some(50.0));
         println!("touch");
     }
 }
