4f9b469626c2efc9bfea94850151f8b3fa1e3612
Michael Hertsens <michael28072000@outlook.com>
2021-04-16 14:00:26 +0200

    revert changes

--- raspian-temp/src/main.rs
+++ raspian-temp/src/main.rs
@@ -15,15 +15,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (button_sender, tm1637_receiver) = mpsc::channel();
     let config: conf::RaspianConfig =
         serde_json::from_str(&args().nth(1).expect("Give in a json configuration."))?;
-    let btn_handler: JoinHandle<Result<(), &'static str>> =
-        handlers::button::get_button_thread_handler(config.get_btn_pin(), button_sender);
-    btn_handler.join().unwrap()?;
-    handlers::four_digit::get_tm_1637_thread(
+    let tm_handler: JoinHandle<Result<(), &'static str>> = handlers::four_digit::get_tm_1637_thread(
         config.get_dio_pin(),
         config.get_clk_pin(),
         config.get_brightness()?,
         tm1637_receiver,
-    )?;
+    );
+    let btn_handler: JoinHandle<Result<(), &'static str>> =
+        handlers::button::get_button_thread_handler(config.get_btn_pin(), button_sender);
+    btn_handler.join().unwrap()?;
+    tm_handler.join().unwrap()?;
 
     Ok(())
 }
