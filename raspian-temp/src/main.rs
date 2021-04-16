ab7011dff6432fc82eb5f2c1f2a4bd7190d9af53
Michael Hertsens <michael28072000@outlook.com>
2021-04-16 14:41:55 +0200

    fix thread

--- raspian-temp/src/main.rs
+++ raspian-temp/src/main.rs
@@ -1,12 +1,12 @@
+use rust_gpiozero::DigitalInputDevice;
 use std::env::args;
 use std::error::Error;
 use std::sync::mpsc;
-use std::thread::JoinHandle;
 use std::sync::mpsc::Sender;
-use rust_gpiozero::DigitalInputDevice;
+use std::thread::JoinHandle;
 
 mod conf;
-mod handlers;
+mod four_digit;
 mod util;
 
 /**
@@ -14,21 +14,22 @@ mod util;
  * app dio_pin_tm clk_pin_tm brightness button_pin
 */
 fn main() -> Result<(), Box<dyn Error>> {
-    let (button_sender, tm1637_receiver) = mpsc::channel();
     let config: conf::RaspianConfig =
         serde_json::from_str(&args().nth(1).expect("Give in a json configuration."))?;
-    let tm_handler: JoinHandle<Result<(), &'static str>> = handlers::four_digit::get_tm_1637_thread(
-        config.get_dio_pin(),
-        config.get_clk_pin(),
-        config.get_brightness()?,
-        tm1637_receiver,
-    );
-    tm_handler.join().unwrap()?;
-    let mut button = DigitalInputDevice::new(config.get_btn_pin();
+    let (button_sender, tm1637_receiver) = mpsc::channel();
+    let mut button = DigitalInputDevice::new(config.get_btn_pin());
     loop {
         button.wait_for_active(None);
         tx.send(()).unwrap();
     }
+    four_digit::get_tm_1637_thread(
+        config.get_dio_pin(),
+        config.get_clk_pin(),
+        config.get_brightness()?,
+        tm1637_receiver,
+    )
+    .join()
+    .unwrap()?;
 
     Ok(())
 }
