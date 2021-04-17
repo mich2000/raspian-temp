15b3f420ed57ff006a0803029de2c4eea3846b68
Michael Hertsens <michael28072000@outlook.com>
2021-04-17 17:28:08 +0200

    update

--- test-buzzer/src/main.rs
+++ test-buzzer/src/main.rs
@@ -1,4 +1,7 @@
 use rppal::gpio::Gpio;
+use std::time::Duration;
+use std::error::Error;
+use std::thread;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pin = Gpio::new()?.get(2)?.into_output();
